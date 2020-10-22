e3e411555e4c5e6f13290bcfc94067953cfb1d42
hexilee <i@hexilee.me>
2020-10-22 16:31:35 +0800

    kubebuilder initialized and add api machine
    Signed-off-by: hexilee <i@hexilee.me>

---
+++ api/v1/machine_types.go
@@ -0,0 +1,89 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type ReserveResources struct {
+	// +kubebuilder:validation:Minimum=0
+
+	// default 1
+	// +optional
+	Cores int32 `json:"cores"`
+
+	// +kubebuilder:validation:Minimum=0
+
+	// default 1 << 30
+	// +optional
+	Memory int64 `json:"memory"`
+}
+
+// MachineSpec defines the desired state of Machine
+type MachineSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Username string `json:"username"`
+
+	Password string `json:"password"`
+
+	Host string `json:"host"`
+
+	// +kubebuilder:validation:Minimum=0
+	// default 22
+	// +optional
+	Port int32 `json:"port"`
+
+	// +optional
+	Reserve *ReserveResources `json:"reserve"`
+}
+
+// MachineStatus defines the observed state of Machine
+type MachineStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Ready *bool `json:"ready"`
+}
+
+// +kubebuilder:object:root=true
+
+// Machine is the Schema for the machines API
+type Machine struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MachineSpec   `json:"spec,omitempty"`
+	Status MachineStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MachineList contains a list of Machine
+type MachineList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Machine `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Machine{}, &MachineList{})
+}
